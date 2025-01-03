a215e4b3cd9da11fa44ba1871f215d1681407524
holden <holden@razrfly.com>
2025-01-03 14:38:50 -0600

    fixes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,20 +1,23 @@
 class QuotesController < ApplicationController
+  include Pagy::Backend
   before_action :authenticate
 
   def index
-    @quotes = Quote.includes(:topic)
-                  .order(created_at: :desc)
-                  .page(params[:page])
-                  .per(25)
+    @pagy, @quotes = pagy(
+      Quote.includes(:topic, :author)
+           .order(created_at: :desc),
+      items: 25
+    )
   end
 
   def search
-    @quotes = Quote.includes(:topic)
-                  .where("content ILIKE ?", "%#{params[:q]}%")
-                  .or(Quote.where("author ILIKE ?", "%#{params[:q]}%"))
-                  .order(created_at: :desc)
-                  .page(params[:page])
-                  .per(25)
+    @pagy, @quotes = pagy(
+      Quote.includes(:topic, :author)
+           .where("content ILIKE ?", "%#{params[:q]}%")
+           .or(Quote.where("attribution_text ILIKE ?", "%#{params[:q]}%"))
+           .order(created_at: :desc),
+      items: 25
+    )
     render :index
   end
 end 
