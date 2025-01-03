0d989c20df3325a9b2544e2200ce6ef787ec9157
holden <holden@razrfly.com>
2025-01-03 14:24:54 -0600

    fix search

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,7 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "./application"
+// Import and register all your controllers from the importmap under controllers/*
+
+import { application } from "controllers/application"
+
+// Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-import PaginationController from "./pagination_controller"
 eagerLoadControllersFrom("controllers", application)
-application.register("pagination", PaginationController)
