9cc3466e4f6e8cd8f342e4089de4c94d298e912b
holden <holden@razrfly.com>
2025-01-16 09:50:36 +0100

    refactor

--- app/controllers/concerns/topics/topic_finder.rb
+++ app/controllers/concerns/topics/topic_finder.rb
@@ -2,11 +2,29 @@ module Topics
   module TopicFinder
     extend ActiveSupport::Concern
 
+    included do
+      before_action :set_topic
+    end
+
     private
 
     def set_topic
-      param_key = params.each_key.find { |k| k.to_s.end_with?('_id') }
-      @topic = Topic.find_by!(slug: params[param_key])
+      param_key = case params[:type]&.downcase
+        when 'concept' then :concept_id
+        when 'place' then :place_id
+        when 'thing' then :thing_id
+        when 'event' then :event_id
+        when 'action' then :action_id
+        when 'other' then :other_id
+        else
+          raise ActionController::ParameterMissing, "Missing topic type parameter"
+      end
+
+      @topic = Topic.friendly.find(params[param_key])
+      
+      unless @topic.type.downcase == params[:type].downcase
+        redirect_to root_path, alert: "Topic not found"
+      end
     end
   end
 end 
--- app/controllers/topics/media/tmdb_search_controller.rb
+++ app/controllers/topics/media/tmdb_search_controller.rb
@@ -1,8 +1,8 @@
 module Topics
   module Media
     class TmdbSearchController < ApplicationController
+      include Topics::TopicFinder
       before_action :authenticate
-      before_action :set_topic
 
       def new
         if params[:query].present?
@@ -43,25 +43,6 @@ module Topics
       end
 
       private
-
-      def set_topic
-        param_key = case params[:type]&.downcase
-          when 'concept' then :concept_id
-          when 'place' then :place_id
-          when 'thing' then :thing_id
-          when 'event' then :event_id
-          when 'action' then :action_id
-          when 'other' then :other_id
-          else
-            raise ActionController::ParameterMissing, "Missing topic type parameter"
-        end
-
-        @topic = Topic.friendly.find(params[param_key])
-        
-        unless @topic.type.downcase == params[:type].downcase
-          redirect_to root_path, alert: "Topic not found"
-        end
-      end
 
       def movie_params
         params.require(:media).permit(:title, :type, :source_id, :source_type, :poster_url, metadata: {})
