8edb743acb17dce1b47420694daef00479ca9353
holden <holden@razrfly.com>
2025-01-16 11:37:47 +0100

    great

--- app/services/knowledge_graph_service.rb
+++ app/services/knowledge_graph_service.rb
@@ -1,22 +1,23 @@
 class KnowledgeGraphService
   include HTTParty
   base_uri 'https://kgsearch.googleapis.com/v1'
-  include ApiCacheable
 
   def self.search(term)
-    Rails.cache.fetch("knowledge_graph/#{term}", expires_in: 1.hour) do
-      response = get('/entities:search', {
-        query: {
-          query: term,
-          types: 'Person',
-          limit: 10,
-          key: Rails.application.credentials.google.knowledge_graph_api_key
-        }
-      })
-      
-      return [] unless response.success?
+    response = get('/entities:search', {
+      query: {
+        query: term,
+        types: 'Person',
+        limit: 25,  # Get more results initially so we can filter
+        key: Rails.application.credentials.google.knowledge_graph_api_key
+      }
+    })
+    
+    return [] unless response.success?
 
-      response['itemListElement'].map do |item|
+    # Process and filter results
+    results = response['itemListElement']
+      .select { |item| item['resultScore'].to_f > 100 }  # Filter low-scoring results
+      .map do |item|
         result = item['result']
         {
           name: result['name'],
@@ -28,6 +29,10 @@ class KnowledgeGraphService
           url: result['url']
         }
       end
-    end
+
+    # Remove duplicates by name and sort by score
+    results.uniq { |r| r[:name] }
+           .sort_by { |r| -r[:score].to_f }
+           .take(5)  # Limit to top 5 results
   end
 end 
